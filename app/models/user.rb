383120d01e5dddd4c93f96efc55d3234ad3fa2a8
kylesallette <kylesallette@gmail.com>
2018-05-11 14:51:25 -0600

    adding user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :articles
+
+  before_save { self.email = email.downcase}
+
   validates_presence_of :username, :email
   validates_uniqueness_of :username
 #  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
